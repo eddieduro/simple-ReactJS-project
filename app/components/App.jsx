3affae060eb9c058054e3c6599a18fb8fb2278b1
eddie <e.jamesduro@gmail.com>
2016-04-19 22:58:14 -0700

    add add functionality for deleting notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -30,7 +30,10 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<button onClick={this.addNote}>+</button>
-				<Notes notes={notes} onEdit={this.editNote} />
+				<Notes 
+					notes={notes} 
+					onEdit={this.editNote} 
+					onDelete={this.deleteNote} />
 			</div>
 		);
 	}
@@ -71,5 +74,14 @@ export default class App extends React.Component {
 		});
 
 		this.setState({notes});
+	};
+
+	deleteNote = (id, e) => {
+		// Avoid bubbling to edit
+		e.stopPropagation();
+
+		this.setState({
+			notes: this.state.notes.filter(note => note.id !== id)
+		});
 	};
 }
